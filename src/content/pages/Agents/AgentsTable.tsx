5c1a5fd21a148326cbd02284340f4124f335c1f0
John Paul Lopez <jpblopez05@gmail.com>
2022-06-15 19:31:43 +0800

    setting up template

---
+++ src/content/pages/Agents/AgentsTable.tsx
@@ -0,0 +1,299 @@
+
+import { FC, ChangeEvent, useState } from 'react';
+import { format } from 'date-fns';
+import numeral from 'numeral';
+import PropTypes from 'prop-types';
+import {
+  Tooltip,
+  Divider,
+  Box,
+  FormControl,
+  InputLabel,
+  Card,
+  Checkbox,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TableContainer,
+  Select,
+  MenuItem,
+  Typography,
+  useTheme,
+  CardHeader
+} from '@mui/material';
+
+import Label from 'src/components/Label';
+import { CryptoOrder, CryptoOrderStatus } from 'src/models/crypto_order';
+import EditTwoToneIcon from '@mui/icons-material/EditTwoTone';
+import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
+import { Agents , AgentStatus } from 'src/models/agents';
+
+
+interface AgentTableProps {
+  className?: string;
+  Agents: Agents[];
+}
+
+interface Filters {
+  status?: AgentStatus;
+}
+
+const getStatusLabel = (cryptoOrderStatus: AgentStatus): JSX.Element => {
+  const map = {
+    disabled: {
+      text: 'Disabled',
+      color: 'error'
+    },
+    active: {
+      text: 'Active',
+      color: 'success'
+    },
+    pending: {
+      text: 'Pending',
+      color: 'warning'
+    }
+  };
+
+  const { text, color }: any = map[cryptoOrderStatus];
+
+  return <Label color={color}>{text}</Label>;
+};
+
+const headCells = [
+  { id: 'name', label: 'First Name' },
+  { id: 'familyName', label: 'Last Name' },
+  { id: 'phoneNumber', label: 'Phone Number' },
+  { id: 'username', label: 'User Name' },
+  { id: 'email', label: 'Email Address' },
+  { id: 'status', label: 'Status' },
+];
+
+
+const applyFilters = (
+  Agents: Agents[],
+  filters: Filters
+): Agents[] => {
+  return Agents.filter((agents) => {
+    let matches = true;
+
+    if (filters.status && agents.status !== filters.status) {
+      matches = false;
+    }
+
+    return matches;
+  });
+};
+
+const applyPagination = (
+  cryptoOrders: Agents[],
+  page: number,
+  limit: number
+): Agents[] => {
+  return cryptoOrders.slice(page * limit, page * limit + limit);
+};
+
+const RecentOrdersTable: FC<AgentTableProps> = ({ Agents }) => {
+  const [selectedCryptoOrders, setSelectedCryptoOrders] = useState<string[]>(
+    []
+  );
+
+  const [page, setPage] = useState<number>(0);
+  const [limit, setLimit] = useState<number>(5);
+  const [filters, setFilters] = useState<Filters>({
+    status: null
+  });
+
+  const statusOptions = [
+    {
+      id: 'active',
+      name: 'Active'
+    },
+    {
+      id: 'disabled',
+      name: 'Disabled'
+    },
+    {
+      id: 'pending',
+      name: 'Pending'
+    },
+  ];
+
+  const handleSelectAllCryptoOrders = (
+    event: ChangeEvent<HTMLInputElement>
+  ): void => {
+    setSelectedCryptoOrders(
+      event.target.checked
+        ? Agents.map((cryptoOrder) => cryptoOrder.firstName)
+        : []
+    );
+  };
+
+  const handleSelectOneCryptoOrder = (
+    event: ChangeEvent<HTMLInputElement>,
+    cryptoOrderId: string
+  ): void => {
+    if (!selectedCryptoOrders.includes(cryptoOrderId)) {
+      setSelectedCryptoOrders((prevSelected) => [
+        ...prevSelected,
+        cryptoOrderId
+      ]);
+    } else {
+      setSelectedCryptoOrders((prevSelected) =>
+        prevSelected.filter((id) => id !== cryptoOrderId)
+      );
+    }
+  };
+
+  const handlePageChange = (event: any, newPage: number): void => {
+    setPage(newPage);
+  };
+
+  const handleLimitChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setLimit(parseInt(event.target.value));
+  };
+
+  const filteredCryptoOrders = applyFilters(Agents, filters);
+  const paginatedCryptoOrders = applyPagination(
+    filteredCryptoOrders,
+    page,
+    limit
+  );
+  const selectedSomeCryptoOrders =
+    selectedCryptoOrders.length > 0 &&
+    selectedCryptoOrders.length < Agents.length;
+  const selectedAllCryptoOrders =
+    selectedCryptoOrders.length === Agents.length;
+  const theme = useTheme();
+
+  return (
+    <Card>
+        <CardHeader
+          title="Agents"
+        />
+      <Divider />
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell padding="checkbox">
+                <Checkbox
+                  color="primary"
+                  checked={selectedAllCryptoOrders}
+                  indeterminate={selectedSomeCryptoOrders}
+                  onChange={handleSelectAllCryptoOrders}
+                />
+              </TableCell>
+              {headCells.map((headCell: any)=>  (
+              <TableCell key={headCell.id}>
+                {headCell.label}
+              </TableCell>
+
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {paginatedCryptoOrders.map((cryptoOrder) => {
+              const isCryptoOrderSelected = selectedCryptoOrders.includes(
+                cryptoOrder.id
+              );
+              return (
+                <TableRow
+                  hover
+                  key={cryptoOrder.id}
+                  selected={isCryptoOrderSelected}
+                >
+                  <TableCell padding="checkbox">
+                    <Checkbox
+                      color="primary"
+                      checked={isCryptoOrderSelected}
+                      onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                        handleSelectOneCryptoOrder(event, cryptoOrder.id)
+                      }
+                      value={isCryptoOrderSelected}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <Typography
+                      variant="body1"
+                      fontWeight="bold"
+                      color="text.primary"
+                      gutterBottom
+                      noWrap
+                    >
+                      {cryptoOrder.firstName}
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography
+                      variant="body1"
+                      fontWeight="bold"
+                      color="text.primary"
+                      gutterBottom
+                      noWrap
+                    >
+                      {cryptoOrder.lastName}
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography
+                      variant="body1"
+                      fontWeight="bold"
+                      color="text.primary"
+                      gutterBottom
+                      noWrap
+                    >
+                      {cryptoOrder.phoneNumber}
+                    </Typography>
+                  </TableCell>
+                  <TableCell >
+                    <Typography
+                      variant="body1"
+                      fontWeight="bold"
+                      color="text.primary"
+                      gutterBottom
+                      noWrap
+                    >
+                      {cryptoOrder.userName}
+
+                    </Typography>
+                  </TableCell>
+                  <TableCell >
+                    {cryptoOrder.emailAddress}
+                  </TableCell>
+                  <TableCell >
+                    {getStatusLabel(cryptoOrder.status)}
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Box p={2}>
+        <TablePagination
+          component="div"
+          count={filteredCryptoOrders.length}
+          onPageChange={handlePageChange}
+          onRowsPerPageChange={handleLimitChange}
+          page={page}
+          rowsPerPage={limit}
+          rowsPerPageOptions={[5, 10, 25, 30]}
+        />
+      </Box>
+    </Card>
+  );
+};
+
+RecentOrdersTable.propTypes = {
+  Agents: PropTypes.array.isRequired
+};
+
+RecentOrdersTable.defaultProps = {
+  Agents: []
+};
+
+export default RecentOrdersTable;
