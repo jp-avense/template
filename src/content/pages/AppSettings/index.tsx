a7296d9b982cf531999bba25a8f2a0e2d355d5cb
euginetan <alwaysonfire2@gmail.com>
2022-08-04 16:02:35 +0800

    Commits for update app settings

--- src/content/pages/AppSettings/index.tsx
+++ src/content/pages/AppSettings/index.tsx
@@ -10,6 +10,8 @@ import Modals from "../Components/Modals";
 import Swal from "sweetalert2";
 import Label from "src/components/Label";
 import ValueModal from "./ValueModal";
+import UpdateAppSettings from "./UpdateAppSettings";
+import UpdateAppSettingsForm from "./UpdateAppSettings/UpdateAppSettingsForm";
 import { settingsService } from "src/services/settings.service";
 
 import { useTranslation } from "react-i18next";
@@ -83,9 +85,26 @@ const AppSettings = () => {
     setSelected(res);
   };
 
-  // const updateForm = useMemo(() => {
-  //   return data.find((item) => item._id === selected[0]);
-  // }, [selected, data]);
+  const updateForm = useMemo(() => {
+    if (selected.length === 1) {
+      const res = data.find((item) => item._id === selected[0]);
+      if (typeof res.value === "object" && Array.isArray(res.value)) {
+        if (typeof res.value[0] === "object") {
+          res.type = "Object";
+        } else if (typeof res.value[0] === "string") {
+          res.type = "Array";
+        }
+      } else if (typeof res.value === "object") {
+        res.type = "Object";
+      } else if (typeof res.value === "string") {
+        res.type = "String";
+      } else if (typeof res.value === "number") {
+        res.type = "Number";
+      }
+
+      return res;
+    }
+  }, [selected, data]);
 
   const handleSelectAll = (e) => {
     if (e.target.checked) {
@@ -186,16 +205,17 @@ const AppSettings = () => {
             handleSelectAll={handleSelectAll}
             action={
               <Box display="flex" flexDirection="row" gap={1}>
-                {/* {selected.length === 1 ? (
+                {selected.length === 1 ? (
                   <>
-                    <UpdateForms>
-                      <UpdateFormField
-                        selectedForm={updateForm}
-                        onFinish={onFinish}
+                    <UpdateAppSettings>
+                      <UpdateAppSettingsForm
+                        data={data}
+                        selected={updateForm}
+                        onDone={onDone}
                       />
-                    </UpdateForms>
+                    </UpdateAppSettings>
                   </>
-                ) : null} */}
+                ) : null}
                 {/* {selected.length ? (
                   <ConfirmModal
                     buttonText={t("delete")}
